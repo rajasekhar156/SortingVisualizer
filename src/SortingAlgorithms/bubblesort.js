5c0c33561920336c1f1e462954c87687fd8bed9c
ITREDDY RAJASEKHAR REDDY <80587161+rajasekhar156@users.noreply.github.com>
2022-07-05 10:27:24 +0530

    Update bubblesort.js

--- src/SortingAlgorithms/bubblesort.js
+++ src/SortingAlgorithms/bubblesort.js
@@ -1,21 +1,18 @@
 const bubblesort = array => {
     const animations = [];
-    for(let i = 0; i < array.length - 1; ++i){
-        for(let j = 0; j < array.length - i - 1; ++j){
-            animations.push([j, j + 1, j - 1]);
-            if(array[j] > array[j + 1]){
+    for(let i = 0; i < array.length - 1; i++){
+        for(let j = 0; j < array.length - i - 1; j++){
+            animations.push([j,j+1,0,0,0]);
+            if(array[j] > array[j+1]){
                 const temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                animations.push([j, j + 1, j, array[j], array[j + 1]]);
-            } else{
-                animations.push([j, j + 1, -1, array[j], array[j + 1]]);
+                array[j] = array[j+1];
+                array[j+1] = temp;
+                animations.push([j,j+1,1,array[j],array[j+1]]);           
             }
-            animations.push([j, j + 1, 0]);
+            animations.push([j,j+1,2,0,0]);
         }
     }
     return animations;
 }
-
 
 export default bubblesort;
